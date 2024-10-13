afcac16fd8e36cb6f588f52e929b402b0769e63f
ivan-private <ivan-private@Ivans-MacBook-Pro.local>
2024-10-13 15:59:53 +0200

    update2024-10

--- dist/js/config.js
+++ dist/js/config.js
@@ -3,8 +3,8 @@ const photos = [
     'photo1.jpg', 'photo2.jpg', 'photo3.jpg', 'photo4.jpg', 'photo5.jpg',
     'photo6.jpg', 'photo7.jpg', 'photo8.jpg', 'photo9.jpg', 'photo10.jpg',
     'photo11.jpg', 'photo12.jpg', 'photo13.jpg', 'photo14.jpg', 'photo15.jpg',
-    'photo16.jpg', 'photo17.jpg', 'photo18.jpg', 'photo19.jpg', 'photo20.jpg',
-    'photo21.jpg', 'photo22.jpg', 'photo23.jpg'
+    'photo16.jpg', 'photo17.jpg', 'photo18.jpg',  'photo19.jpg','photo20.jpg',
+    'photo21.jpg', 'photo22.jpg', 'photo23.jpg', 'photo24.jpg'
 ];
 
-const descriptions = ['Srnci', 'Jelen', 'Rarity - srnci', 'Srnec', 'Srnec s lýčím', 'Muflon', 'Lebka', 'Jelen', 'Věšák', 'Věšák', 'Hlavy před úpravou', 'Hlavy po úpravě', 'Raritní kus', 'Jeleni parohy', 'Trofej','Hlavy před úpravou', 'Hlavy po úpravě', 'Trofej','Trofej','Trofej','Trofej','Trofej','Trofej'];
+const descriptions = ['Srnci', 'Jelen', 'Raritní srnci', 'Srnec', 'Srnec v lýčí', 'Muflon', 'Daněk', 'Jelen', 'Lovecký věšák', 'Lovecký věšák', 'Trofeje před úpravou', 'Trofeje po úpravě', 'Raritní jelen', 'Jelení trofeje', 'Jelen sika','Hlavy před úpravou', 'Hlavy po úpravě', 'Jelen mnich','Trofej daněk','Trofej lišky','Trofej nutrie', 'Trofej bachyně', 'Nález srnec před úpravou','Nález srnec po úpravě'];
